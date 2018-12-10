8bc27c50e983122b5641cf8f8fd1fee35f7bd433
liyangyang-pc <liyangyang@liyangyangdeMacBook-Air.local>
2018-12-10 16:01:56 +0800

    第二次更行

--- text/ViewController.m
+++ text/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor redColor];
+    UILabel *label = [UILabel new];
+    label.text  = @"0.01.1";
+    [self.view addSubview:label];
     
 }
 

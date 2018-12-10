15679a7377348ee79294c7ce8e7302876fc01e53
李的 <liyangyang@liyangyangdemacbook-air.local>
2018-12-10 11:50:42 +0800

    Initial Commit

---
+++ text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by liyangyang-pc on 2018/12/10.
+//  Copyright © 2018年 254327737@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by liyangyang-pc on 2018/12/10.
+//  Copyright © 2018年 254327737@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
